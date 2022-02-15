e1c15f6d1ccad1e69557783bd1e076a922f625af
aaromero <aaromero17@gmail.com>
2022-02-15 13:53:11 -0400

    laravel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -10,9 +11,22 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+|ghg
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+/*
+Route::get('/home/{id}', 'Ejemplo3Controller@index');
+  
+
+
+Route::get('/', 'PaginasController@index');
+Route::get('/index', 'PaginasController@index');
+Route::get('/quienesSomos', 'PaginasController@quienesSomos');
+Route::get('/dondeEstamos', 'PaginasController@dondeEstamos');
+Route::get('/foros', 'PaginasController@foros');
+*/
+
+Route::resource("posts","Ejemplo3Controller");
